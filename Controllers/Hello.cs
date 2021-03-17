49c48942e20004e14964a4847351d6e9e7fb48e1
softwaiter <softwaiter@126.com>
2021-03-17 18:30:36 +0800

    增加大写Controls和Services目录

---
+++ Controllers/Hello.cs
@@ -0,0 +1,15 @@
+﻿using CodeM.FastApi.Context;
+using System.Threading.Tasks;
+
+namespace CodeM.FastApi.Controllers
+{
+    public class Hello
+    {
+
+        public async Task Handle(ControllerContext cc)
+        {
+            await cc.JsonAsync("Hello World.");
+        }
+
+    }
+}
