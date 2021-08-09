3ed60ddd5d54c59245f66768197007e0dc970f3c
softwaiter <softwaiter@126.com>
2021-08-09 17:07:23 +0800

    :sparkles:增加定时任务处理模块

---
+++ libs/Schedule/ScheduleManager.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeM.FastApi.Schedule
+{
+    public class ScheduleManager
+    {
+        public static void Load()
+        {
+        }
+
+        public static void Reload()
+        {
+        }
+
+        public static bool Start()
+        {
+            return true;
+        }
+
+        public static bool Pause()
+        {
+            return true;
+        }
+
+        public static bool Resume()
+        {
+            return true;
+        }
+
+        public static bool Stop()
+        {
+            return true;
+        }
+    }
+}
