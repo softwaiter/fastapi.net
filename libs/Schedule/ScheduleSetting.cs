f7dc4a9c3df9bc02885f4b9883df1a6713fd4c27
softwaiter <softwaiter@126.com>
2021-08-10 08:41:24 +0800

    :construction:增加Schedule配置信息类

---
+++ libs/Schedule/ScheduleSetting.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeM.FastApi.Schedule
+{
+    public class ScheduleSetting
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 重复次数，默认0，不限制
+        /// </summary>
+        public int Repeat { get; set; } = 0;
+
+        /// <summary>
+        /// 单位：秒
+        /// </summary>
+        public int Interval { get; set; }
+
+        /// <summary>
+        /// Cron表达式
+        /// </summary>
+        public string Cron { get; set; }
+    }
+}
