1a3abd5908d4ec32f906f46cc7e48bf750bf6c48
softwaiter <softwaiter@126.com>
2021-10-22 14:00:46 +0800

    :construction:清理无用代码

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,12 +1,9 @@
 ﻿using CodeM.FastApi.System.Core;
-using CodeM.FastApi.Context;
 
 namespace CodeM.FastApi.Controllers
 {
     public class BaseController
     {
-        ControllerContext mabc;
-
         public App App
         {
             get
