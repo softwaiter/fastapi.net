59f190d9d8065cab64ad1a5e6a202c78d2439f5c
softwaiter <softwaiter@126.com>
2021-10-27 09:20:40 +0800

    :art:增加Service基类BaseService

---
+++ Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using CodeM.FastApi.System.Core;
+
+namespace CodeM.FastApi.Services
+{
+    public class BaseService
+    {
+        public App App
+        {
+            get
+            {
+                return App.GetInstance();
+            }
+        }
+    }
+}
