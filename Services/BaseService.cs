1a9e5d385e57b52cc2aaa43a4a7c2d47faf96d20
softwaiter <softwaiter@126.com>
2021-10-28 11:46:34 +0800

    :construction:BaseService基类增加Service方法，用于获取其他Service实例

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -1,9 +1,27 @@
-﻿using CodeM.FastApi.System.Core;
+﻿using CodeM.Common.Ioc;
+using CodeM.FastApi.System.Core;
+using System.Text.RegularExpressions;
 
 namespace CodeM.FastApi.Services
 {
     public class BaseService
     {
+        private static Regex sLastDot = new Regex("\\.[^\\.]*$");
+
+        public dynamic Service(string serviceName, bool singleton = true)
+        {
+            string serviceFullName = GetType().FullName;
+            serviceFullName = sLastDot.Replace(serviceFullName, string.Concat(".", serviceName, "Service"));
+            if (singleton)
+            {
+                return IocUtils.GetSingleObject(serviceFullName);
+            }
+            else
+            {
+                return IocUtils.GetObject(serviceFullName);
+            }
+        }
+
         public App App
         {
             get
