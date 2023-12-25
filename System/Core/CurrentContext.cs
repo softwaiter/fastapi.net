bcae8e9c06b52b55c04cb6040247525978a3bbd6
softwaiter <softwaiter@126.com>
2023-12-25 21:29:00 +0800

    :construction:增加全局获取当前请求上下文功能

---
+++ System/Core/CurrentContext.cs
@@ -0,0 +1,40 @@
+﻿using CodeM.FastApi.Context;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace CodeM.FastApi.System.Core
+{
+    public class CurrentContext
+    {
+        private static ConcurrentDictionary<int, HttpContext> sRequests = new ConcurrentDictionary<int, HttpContext>();
+
+        public static void Add(HttpContext context)
+        {
+            int procId = Thread.GetCurrentProcessorId();
+            sRequests.TryAdd(procId, context);
+        }
+
+        public static ControllerContext Context
+        {
+            get
+            {
+                HttpContext context = null;
+                int procId = Thread.GetCurrentProcessorId();
+                if (sRequests.TryGetValue(procId, out context))
+                {
+                    return ControllerContext.FromHttpContext(context,
+                        Application.Instance().Config());
+                }
+                return null;
+            }
+        }
+
+        public static void Remove()
+        {
+            HttpContext context;
+            int procId = Thread.GetCurrentProcessorId();
+            sRequests.TryRemove(procId, out context);
+        }
+    }
+}
---
+++ System/Middlewares/ContextMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using CodeM.FastApi.Config;
+using CodeM.FastApi.System.Core;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CodeM.FastApi.System.Middlewares
+{
+    public class ContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private ApplicationConfig mConfig;
+
+        public ContextMiddleware(RequestDelegate next, ApplicationConfig config)
+        {
+            _next = next;
+            mConfig = config;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            CurrentContext.Add(context);
+
+            await _next(context);
+
+            CurrentContext.Remove();
+        }
+    }
+}
